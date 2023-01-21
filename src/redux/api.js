53b5286b9cfc5c4f2e1e6e53971578525fa5f57d
karthikeyan <kkarthikeyanre@gmail.com>
2023-01-21 14:54:47 +0530

    backennd_add_commit

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://i-mdb-2-0-server-git-main-karthikeyankanniappan.vercel.app",
 });
 
 export const getMovie = () => {
